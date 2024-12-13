218d15c15c390d802193cd488d114f94e84e2ed2
Sarah Hannes <78901374+SarahHannes@users.noreply.github.com>
2024-12-13 15:26:35 +0900

    Update hello-with-reqs.py

--- hello-with-reqs.py
+++ hello-with-reqs.py
@@ -1,6 +1,6 @@
-print("this is printing from hello-with-pandas.py")
+print("this is printing from hello-with-pandas from git!!!.py")
 
 import pandas as pd
 
 df = pd.DataFrame({"col1": ["a", "b"],
-              "col2": ["c", "d"]}, index=[0,1]).to_csv("csvfrompandas.csv")
+              "col2": ["c", "d"]}, index=[0,1]).to_csv("csvfrompandas_fromgit.csv")
